701ed0c39cba6b9285980bccf7bc9c204f1c5695
Hugo Zapata <hugo@hugozap.com>
2024-02-16 11:39:28 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,16 @@ import pandas as pd
 import streamlit as st
 
 
-uploaded_files = st.file_uploader("Upload an Excel File", type="xlsx")
 if uploaded_files is not None:
     excel_data = pd.read_excel(uploaded_files)
+    # Add 'llave' column by slicing the first 3 characters of the 'item' column
+    excel_data['llave'] = excel_data['item'].astype(str).str[:3]
 else:
     excel_data = pd.DataFrame()
 
 # Process and display the data in Streamlit
 if len(excel_data) > 0:
     st.title('Excel Data Processing App')
-    st.write(excel_data)
+    st.write(excel_data.head())  # Adjust the number in head() if you want to display more rows
 else:
     st.warning("No Excel file uploaded. Please upload a valid XLSX file to continue.")
-
